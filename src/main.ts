59fd9752eed3b73ea5469115e48521b57cd2d08e
Michael Flood <michaelflood@Michaels-MacBook-Pro.local>
2019-05-12 14:11:20 +0100

    contact form validation and POT

--- src/main.ts
+++ src/main.ts
@@ -67,3 +67,103 @@ headers.forEach(header => setupBurgerClickListeners(`burgerMenu${header}`));
 // mobile dropdown should always close when they click a link
 const allLinks = document.querySelectorAll(navLinkSelector);
 allLinks.forEach(link => link.addEventListener('click', () => burgerOpen && toggleBurgerMenuOpen()));
+
+
+// CONTACT FORM //////////////////////////////////////////////////////////////////////////////////////////////
+const contactForm = document.forms['contactForm'];
+const submitButton = <HTMLInputElement> document.getElementById('contactFormButton');
+const contactFormUrl = 'http://numbersapi.com/random/math?min=0&max=20&json';
+
+interface ContactPayload {
+	name: string;
+	email: string;
+	message: string;
+}
+
+const checkContactForm = () => {
+	let formValid = true;
+	for (var i = 0; i < contactForm.elements.length; i++) {  // can't use forEach as not an array
+		if (!contactForm.elements[i].checkValidity()) {
+			formValid = false;
+		}
+	}
+	submitButton.disabled = !formValid;
+}
+
+const nameInput = <HTMLInputElement> document.getElementById('nameInput');
+const emailInput = <HTMLInputElement> document.getElementById('emailInput');
+const messageInput = <HTMLInputElement> document.getElementById('messageInput');
+
+const submitContactForm = () => {
+	toggleLoadingSpinner();
+	const xhttp = new XMLHttpRequest();
+	const payload = buildPayload();
+	xhttp.onreadystatechange = function() {
+	    if (this.readyState == 4) {
+	       if (this.status == 200) {
+	       	handleSubmitSuccess()
+	       }
+	       else {
+	       	handleSubmitFailure(this.response);
+	       }
+	    }
+	};
+	xhttp.open("GET", contactFormUrl, true);
+	xhttp.send();
+}
+
+const wipeForm = () => {
+	for (var i = 0; i < contactForm.elements.length; i++) {  // can't use forEach as not an array
+		contactForm.elements[i].value = '';
+	}
+	checkContactForm();
+}
+
+const toggleLoadingSpinner = () => {
+	const spinner = document.getElementById('loadingSpinner');
+	spinner.classList.toggle('hidden');
+	spinner.classList.toggle('flex');
+}
+
+const buildPayload = (): ContactPayload => {
+	return {
+		name: nameInput.value,
+		email: emailInput.value,
+		message: messageInput.value,
+	}
+}
+
+const handleSubmitSuccess = () => {
+	wipeForm();
+	toggleLoadingSpinner();
+	showPopup('Thanks!', 'Thanks for your enquiry - I\'ll be in touch soon!');
+}
+
+const handleSubmitFailure = (resp) => {
+	toggleLoadingSpinner();
+	showPopup('Error', resp);
+}
+
+// add event listeners for input and button
+for (var i = 0; i < contactForm.elements.length; i++) {  // can't use forEach as not an array
+	contactForm.elements[i].addEventListener('keyup', () => checkContactForm());
+}
+submitButton.addEventListener('click', () => submitContactForm());
+
+
+// POPUPS ////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const togglePopup = () => {
+	const spinner = document.getElementById('contactPopup');
+	spinner.classList.toggle('hidden');
+	spinner.classList.toggle('flex');
+}
+
+const showPopup = (title: string, message: string) => {
+	document.getElementById('popupTitle').innerHTML = title;
+	document.getElementById('popupMessage').innerHTML = message;
+	togglePopup();
+}
+
+document.getElementById('popupButton').addEventListener('click', () => togglePopup());
+
